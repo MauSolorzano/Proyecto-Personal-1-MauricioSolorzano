44b0f17ff5f95be174230d888113ffffde237992
MauSolorzano <127962979+MauSolorzano@users.noreply.github.com>
2023-05-30 15:16:49 -0600

    Cambio del 30-05-23
    Anadir footer y css menu

---
+++ js/footer.js
@@ -0,0 +1,22 @@
+contenidoFooter.innerHTML += `
+
+<footer class="footer bg-light text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="footer-text">Progreso Seguro</h2>
+                    <p class="footer-text">
+                    Progreso Seguro es una prestigiosa institución académica comprometida con el éxito y el desarrollo
+                    integral de sus estudiantes. Nuestro enfoque se basa en proporcionar una educación de calidad que prepare
+                    a los estudiantes para enfrentar los desafíos del mundo actual.
+                    </p>
+                </div>
+
+
+            </div>
+            <hr>
+            <p class="footer-copy">&copy; 2023 Progreso Seguro. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+
+`; 
